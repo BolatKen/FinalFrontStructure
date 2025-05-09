d16bf41bbe9a3afec44341143be60ba7a4847cc0
Assylzhan-Izbassar <Assylzhan-Izbassar>
2025-05-09 18:25:08 +0500

    added Configurator and Description

--- src/components/ui/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ui/ButtonPrimary/ButtonPrimary.jsx
@@ -1,9 +1,25 @@
 import styles from './ButtonPrimary.module.css';
 
-export default function ButtonPrimary({ children, onClick }) {
+export default function ButtonPrimary({
+    children,
+    onClick,
+    isSelected = false,
+    isPadding = false,
+    isNotification = false
+}) {
+    let notification = ''
+    if (isNotification) {
+        notification = <div className="icon__notification">
+            <img src="/icons/check.png" alt="check-icon" />
+        </div>
+    }
     return (
-        <div className='btn' onClick={onClick}>
-            <button className={styles.btn__item} >{children}</button>
+        <div className='btn icon' onClick={onClick}>
+            <button className={`${styles.btn__item} 
+            ${styles.btn__item_padding ? isPadding : ''}
+            ${styles.btn__item_selected ? isSelected : ''}
+            `} >{children}</button>
+            {notification}
         </div>
     );
 }

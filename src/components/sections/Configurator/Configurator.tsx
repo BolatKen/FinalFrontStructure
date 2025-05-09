8f94f49a4d41f9a2042b8958fb67b427ac568e72
Assylzhan-Izbassar <Assylzhan-Izbassar>
2025-05-09 18:25:08 +0500

    added Configurator and Description

--- src/components/sections/Configurator/Configurator.tsx
+++ src/components/sections/Configurator/Configurator.tsx
@@ -1,29 +1,101 @@
-import ColorSelector from "../../ui/ColorItem/ColorItem";
-import { ButtonPrimary } from '../../ui/ButtonPrimary/ButtonPrimary';
 import styles from "./Configurator.module.css";
+import { ButtonPrimary } from '../../ui/ButtonPrimary/ButtonPrimary';
+import { ButtonOrange } from '../../ui/ButtonOrange/ButtonOrange';
+import { ColorConfigureEdit } from '../../shared/ColorConfigureEdit/ColorConfigureEdit';
 
 export default function Configurator() {
+  const productItemBtns = [
+    {
+      children: 'Подлокотники',
+      onClick: null,
+      isSelected: true,
+      isPadding: true,
+      isNotification: true
+    },
+    {
+      children: 'Без подлокотника',
+      onClick: null,
+      isSelected: false,
+      isPadding: true,
+      isNotification: false
+    }
+  ]
+
+  const colorConfigure = [
+    {
+      configureTitle: 'Обивка',
+      configureVal: 'Черный, экокожа',
+      colorData: [
+        {
+          'imageUrl': '/textures/material-black.png',
+          'altText': 'Черный',
+          'isSelected': true
+        },
+        {
+          'imageUrl': '/textures/material-green.png',
+          'altText': 'Зеленый',
+          'isSelected': false
+        },
+        {
+          'imageUrl': '/textures/material-blue.png',
+          'altText': 'Синий',
+          'isSelected': false
+        },
+        {
+          'imageUrl': '/textures/material-pink.png',
+          'altText': 'Розовый',
+          'isSelected': false
+        }
+      ]
+    },
+    {
+      configureTitle: 'Каркас',
+      configureVal: 'Темное дерево',
+      colorData: [
+        {
+          'imageUrl': '/textures/wood-dark.png',
+          'altText': 'Темное дерево',
+          'isSelected': true
+        },
+        {
+          'imageUrl': '/textures/wood-white.png',
+          'altText': 'Светлое дерево',
+          'isSelected': false
+        },
+      ]
+    }
+  ]
+
   return (
-    <section className={[styles.configurator, '_container'].join()}>
-      <div className={styles.configurator__main}>
-        {/* Фото товара */}
-        <div className={[styles.configurator__image, '_img'].join(" ")}>
-          <img src="/core/2.png" alt="Кресло Aurora" />
-        </div>
-        {/* Панель выбора */}
-        <div className={styles.configurator__panel}>
-          <h2 className={styles.configurator__title}>Конфигуратор</h2>
-
-          <div className={styles.configurator__icon}>
-            <p className={styles.configurator__text}>
-              Создайте уникальную мебель, отражающую ваш <br /> вкус и
-              интерьер
-            </p>
+    <div className={styles.configurator}>
+      <section className={[styles.configurator__inner, '_container'].join(" ")}>
+        <div className={styles.configurator__main}>
+          {/* Фото товара */}
+          <div className={[styles.configurator__image, '_img'].join(" ")}>
+            <img src="/core/2.png" alt="Кресло Aurora" />
           </div>
+          {/* Панель выбора */}
+          <div className={styles.configurator__panel}>
+            <h2 className={styles.configurator__title}>Конфигуратор</h2>
 
-          {/* Радиокнопки */}
-          <div className="configurator__group group">
-            <div className="group__button btn icon">
+            <div className={styles.configurator__icon}>
+              <p className={styles.configurator__text}>
+                Создайте уникальную мебель, отражающую ваш <br /> вкус и
+                интерьер
+              </p>
+            </div>
+
+            {/* Радиокнопки */}
+            <div className={[styles.configurator__group, styles.group].join(" ")}>
+              {productItemBtns.map((item, idx) => (
+                <ButtonPrimary key={idx}
+                  children={item.children}
+                  onClick={item.onClick}
+                  isSelected={item.isSelected}
+                  isNotification={item.isNotification}
+                  isPadding={item.isPadding} />
+              ))}
+              {/* <div className="group__button btn icon">
               <button className="btn__item btn__item_padding btn__item_selected">
                 Подлокотники
               </button>
@@ -35,92 +107,35 @@ export default function Configurator() {
               <button className="btn__item btn__item_padding">
                 Без подлокотника
               </button>
+            </div> */}
             </div>
-          </div>
 
-          {/* Обивка */}
-          <div className="configurator__option color variant">
-            <div className="variant__title">
-              <div className="variant__text">Обивка</div>
-              <div className="variant__subtext">Черный, экокожа</div>
-            </div>
-            <div className="variant__inner">
-              <ul className="color__items">
-                <li className="color__item color__item_selected">
-                  <ColorSelector
-                    imageUrl="/textures/material-black.png"
-                    alt="Черный"
-                    selected
-                  />
-                </li>
-                <li className="color__item">
-                  <ColorSelector
-                    imageUrl="/textures/material-green.png"
-                    alt="Зеленый"
-                  />
-                </li>
-                <li className="color__item">
-                  <ColorSelector
-                    imageUrl="/textures/material-blue.png"
-                    alt="Синий"
-                  />
-                </li>
-                <li className="color__item">
-                  <ColorSelector
-                    imageUrl="/textures/material-pink.png"
-                    alt="Розовый"
-                  />
-                </li>
-              </ul>
-              <ButtonPrimary children={"Под заказ"} onClick={null} />
-            </div>
-          </div>
+            {/* Обивка */}
 
-          {/* Каркас */}
-          <div className="configurator__option color variant">
-            <div className="variant__title">
-              <div className="variant__text">Каркас</div>
-              <div className="variant__subtext">Темное дерево</div>
-            </div>
-            <div className="variant__inner">
-              <ul className="color__items">
-                <li className="color__item color__item_selected">
-                  <ColorSelector
-                    imageUrl="/textures/wood-dark.png"
-                    alt="Темное дерево"
-                    selected
-                  />
-                </li>
-                <li className="color__item">
-                  <ColorSelector
-                    imageUrl="/textures/wood-white.png"
-                    alt="Светлое дерево"
-                  />
-                </li>
-              </ul>
-              <ButtonPrimary children={"Под заказ"} onClick={null} />
-            </div>
-          </div>
+            {colorConfigure.map((item, idx) => (
+              <ColorConfigureEdit configureTitle={item.configureTitle}
+                configureVal={item.configureVal}
+                colorData={item.colorData} />
+            ))}
 
-          {/* Кнопка под заказ */}
-          <div className="configurator__btn btn">
-            <button className="btn__action">Под заказ</button>
+            {/* Кнопка под заказ */}
+            <ButtonOrange children={'Под заказ'} onClick={null} type={"button"} />
           </div>
         </div>
-      </div>
 
-      {/* Описание */}
-      <div className={[styles['configurator__description'], styles['configurator-text']].join(" ")}>
-        <h3 className={styles['configurator-text__title']}>Aurora</h3>
-        <p className={styles['configurator-text__desc']}>
-          «Бусиновский парк» расположен на севере Москвы — в районе Западное
-          Дегунино, в 20 минутах пешком от метро «Ховрино» и 15 от МЦД
-          «Грачёвская». Яркий и современный образ жилого квартала создают
-          башни- доминанты — у каждой из них свой уникальный фасад,
-          разработанный российскими архитектурными бюро.
-        </p>
-      </div>
-    </section>
+        {/* Описание */}
+        <div className={[styles['configurator__description'], styles['configurator-text']].join(" ")}>
+          <h3 className={styles['configurator-text__title']}>Aurora</h3>
+          <p className={styles['configurator-text__desc']}>
+            «Бусиновский парк» расположен на севере Москвы — в районе Западное
+            Дегунино, в 20 минутах пешком от метро «Ховрино» и 15 от МЦД
+            «Грачёвская». Яркий и современный образ жилого квартала создают
+            башни- доминанты — у каждой из них свой уникальный фасад,
+            разработанный российскими архитектурными бюро.
+          </p>
+        </div>
+      </section>
+    </div>
 
   );
 }
---
+++ src/components/shared/ColorConfigureEdit/ColorConfigureEdit.jsx
@@ -0,0 +1,20 @@
+import styles from './ColorConfigureEdit.module.css';
+import { ColorList } from '../../ui/ColorList/ColorList';
+import { ButtonPrimary } from '../../ui/ButtonPrimary/ButtonPrimary';
+
+export default function ColorConfigureEdit({ configureTitle, configureVal, colorData }) {
+    return (
+        <div className={[styles.configure__option, styles.variant].join(" ")}>
+            <div className={styles.variant__title}>
+                <div className={styles.variant__text}>{configureTitle}</div>
+                <div className={styles.variant__subtext}>{configureVal}</div>
+            </div>
+            <div className={styles.variant__inner}>
+                <ColorList colorData={colorData} />
+                <ButtonPrimary children={"Под заказ"} onClick={null} />
+            </div>
+        </div>
+    );
+};
+
+export { default as ColorConfigureEdit } from './ColorConfigureEdit';

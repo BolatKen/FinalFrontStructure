a116f06e4abacf0f7f7b10962f12016d7a8f23e6
yanekissayabully <aripov.cr7@gmail.com>
2025-05-10 18:29:31 +0500

    productCart+bd

--- src/app/catalog/products/[slug]/page.tsx
+++ src/app/catalog/products/[slug]/page.tsx
@@ -1,23 +1,39 @@
-// app/product/[slug]/page.tsx
-import { getProductBySlug } from '@/services/product.service'
-import Welcome from '@/components/sections/Welcome/Welcome'
-import Description from '@/components/sections/Description/Description'
-import Configurator from '@/components/sections/Configurator/Configurator'
+// // app/product/[slug]/page.tsx
+// import { getProductBySlug } from '@/services/product.service'
+// import Welcome from '@/components/sections/Welcome/Welcome'
+// import Description from '@/components/sections/Description/Description'
+// import Configurator from '@/components/sections/Configurator/Configurator'
 
-interface ProductPageProps {
-  params: { slug: string }
-}
+// interface ProductPageProps {
+//   params: { slug: string }
+// }
 
-export default async function ProductPage({ params }: ProductPageProps) {
-  const product = await getProductBySlug(params.slug)
-  console.log(product)
-  if (!product) {
-    return <div>Товар не найден</div>
-  }
+// export default async function ProductPage({ params }: ProductPageProps) {
+//   const product = await getProductBySlug(params.slug)
+//   console.log(product)
+//   if (!product) {
+//     return <div>Товар не найден</div>
+//   }
 
-  return (<>
-    <Welcome product={product} />
-    <Configurator />
-    <Description product={product} />
-  </>)
+//   return (<>
+//     <Welcome product={product} />
+//     <Configurator />
+//     <Description product={product} />
+//   </>)
+// }
+
+
+import { getAllProducts } from "@/services/product.service";
+import BestOffers from "@/components/sections/BestOffers/BestOffers";
+
+export default async function Page() {
+  const products = await getAllProducts(); // Загружаем товары через твой сервис
+
+  return (
+    <main>
+      {/* Тут твой контент */}
+      
+      <BestOffers products={products} /> {/* Передаёшь товары сюда */}
+    </main>
+  );
 }

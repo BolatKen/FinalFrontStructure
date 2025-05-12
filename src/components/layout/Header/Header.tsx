212757c40742664cee8c02b68bc1066b3dcb1f4c
yanekissayabully <aripov.cr7@gmail.com>
2025-05-12 17:51:45 +0500

    korzina

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,16 +1,80 @@
+// import { HeaderItem } from "../../shared/HeaderItem/HeaderItem";
+// import styles from "./Header.module.css";
+
+// export default function Header() {
+//   const headerItems = [
+//     "Кресла",
+//     "Столы",
+//     "Диваны",
+//     "Мойки",
+//     "Стулья",
+//     "Шкафы",
+//     "Другая мебель",
+//   ];
+
+//   return (
+//     <header className={styles.header}>
+//       <div className={`${styles.header__inner} _container-bigger`}>
+//         <div className={styles.header__logo}>Leka Beauty</div>
+
+//         <ul className={styles.header__items}>
+//           {headerItems.map((item, idx) => (
+//             <HeaderItem key={idx} children={item} onClick={null} />
+//           ))}
+//         </ul>
+
+//         <div className={`${styles.header__cart} ${styles.cart} icon`}>
+//           <div className={styles.cart__icon}>
+//             <div className={styles.cart__rect}></div>
+//             <img
+//               className={styles.cart__svg}
+//               src="/icons/cart-icon.png"
+//               alt="shopping-cart-line"
+//             />
+//           </div>
+//           <div className="icon__notification">1</div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+
+"use client";
+
+import { useEffect, useState } from "react";
 import { HeaderItem } from "../../shared/HeaderItem/HeaderItem";
 import styles from "./Header.module.css";
 
 export default function Header() {
-  const headerItems = [
-    "Кресла",
-    "Столы",
-    "Диваны",
-    "Мойки",
-    "Стулья",
-    "Шкафы",
-    "Другая мебель",
-  ];
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const storedData = localStorage.getItem("cartData");
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        const now = new Date().getTime();
+        if (now - parsedData.timestamp < 24 * 60 * 60 * 1000) {
+          setCartCount(parsedData.count);
+        } else {
+          localStorage.removeItem("cartData");
+          setCartCount(0);
+        }
+      } else {
+        setCartCount(0);
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    // Инициализация при старте
+    handleStorageChange();
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -18,7 +82,7 @@ export default function Header() {
         <div className={styles.header__logo}>Leka Beauty</div>
 
         <ul className={styles.header__items}>
-          {headerItems.map((item, idx) => (
+          {["Кресла", "Столы", "Диваны", "Мойки", "Стулья", "Шкафы", "Другая мебель"].map((item, idx) => (
             <HeaderItem key={idx} children={item} onClick={null} />
           ))}
         </ul>
@@ -32,9 +96,12 @@ export default function Header() {
               alt="shopping-cart-line"
             />
           </div>
-          <div className="icon__notification">1</div>
+          {cartCount > 0 && (
+            <div className="icon__notification">{cartCount}</div>
+          )}
         </div>
       </div>
     </header>
   );
 }
+
--- src/components/sections/Welcome/Welcome.tsx
+++ src/components/sections/Welcome/Welcome.tsx
@@ -435,6 +435,216 @@
 
 
 
+// "use client"
+// import React, { useState } from 'react';
+// import styles from "./Welcome.module.css";
+// import ColorConfigure from "../../shared/ColorConfigure/ColorConfigure";
+// import ButtonPrimary from '../../ui/ButtonPrimary/ButtonPrimary';
+// import Arrow from '../../ui/Arrow/Arrow';
+// import ButtonOrange from '../../ui/ButtonOrange/ButtonOrange';
+// import ProductScene from "@/components/explosion/product-scene";
+// import BonusValue from '../../ui/BonusValue/BonusValue';
+
+// interface VariantOption {
+//   id: number;
+//   option_display: {
+//     id: number;
+//     option_type_display: string;
+//     value: string;
+//     code: string;
+//   };
+// }
+
+// interface Variant {
+//   id: number;
+//   sku: string;
+//   price: string;
+//   old_price?: string | null;
+//   currency: string;
+//   variant_options: VariantOption[];
+// }
+
+// interface Product {
+//   id: number;
+//   name: string;
+//   description: string;
+//   base_sku: string;
+//   slug: string;
+//   category: string;
+//   model_url: string;
+//   variants: Variant[];
+// }
+
+// interface WelcomeProps {
+//   product: Product;
+// }
+
+// export default function Welcome({ product }: WelcomeProps) {
+//   const mainVariant = product.variants[0];
+
+//   const colorsAndMaterials = [
+//     {
+//       configureTitle: "Цвет и материал",
+//       colorData: [
+//         { color: 'Желтый', code: '#bfb7ae', imageUrl: "/textures/material-yellow.png", altText: "Желтая кожа", isSelected: true },
+//         { color: 'Белый', code: '#e2e2e2', imageUrl: "/textures/material-white.png", altText: "Белая кожа", isSelected: false },
+//         { color: 'Коричневый', code: '#7b716d', imageUrl: "/textures/material-brown.png", altText: "Коричневая кожа", isSelected: false }
+//       ]
+//     },
+//     {
+//       configureTitle: "Каркас",
+//       colorData: [
+//         { color: 'Белое', code: '#bfb7ae', imageUrl: "/textures/wood-white.png", altText: "Белое дерево", isSelected: true }
+//       ]
+//     }
+//   ];
+
+//   const titleBlock = product.name.length > 13
+//     ? (
+//       <div className={`${styles.welcome__title} ${styles.welcome__title_long}`}>
+//         {product.name.split(',')[0]}
+//       </div>
+//     )
+//     : (
+//       <div className={`${styles.welcome__title} ${styles.welcome__title_short}`}>
+//         {product.name}
+//       </div>
+//     );
+
+//   const [selectedView, setSelectedView] = useState<'gallery' | '3d'>('gallery');
+//   const [selectedColor, setSelectedColor] = useState<string | null>(null);
+
+//   const galleryImages = [
+//     product.model_url || "/core/1.png",
+//     "/core/2.png",
+//     "/core/3.png",
+//     "/core/4.png",
+//     // "/core/5.jpg",
+//   ];
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   const handlePrevImage = () => {
+//     setCurrentImageIndex((prev) =>
+//       prev === 0 ? galleryImages.length - 1 : prev - 1
+//     );
+//   };
+
+//   const handleNextImage = () => {
+//     setCurrentImageIndex((prev) =>
+//       prev === galleryImages.length - 1 ? 0 : prev + 1
+//     );
+//   };
+
+//   const handleSelectImage = (index: number) => {
+//     setCurrentImageIndex(index);
+//   };
+
+//   return (
+//     <main className={styles.welcome}>
+//       <div className={styles.welcome__product}>
+//         <div className={`${styles.welcome__inner} _container-bigger`}>
+//           {/* Стрелка влево */}
+//           {selectedView === 'gallery' && (
+//             <Arrow onClick={handlePrevImage} />
+//           )}
+
+//           <div className={`${styles.welcome__container} _container`}>
+//             {selectedView === 'gallery' && (
+//               <>
+//                 {titleBlock}
+//                 <div className={styles.welcome__img}>
+//                   <img
+//                     className="welcome__item"
+//                     src={galleryImages[currentImageIndex]}
+//                     alt={`gallery-image-${currentImageIndex}`}
+//                   />
+//                 </div>
+//               </>
+//             )}
+//             {selectedView === '3d' && (
+//               <ProductScene color={selectedColor} />
+//             )}
+
+//             <div className={`${styles.welcome__info} ${styles.info} ${selectedView === '3d' ? styles.welcome__info_margin : ''}`}>
+//               <div className={`${styles.info__option} ${styles.option}`}>
+//                 <div
+//                   className={`${styles.option__item} ${selectedView === 'gallery' ? styles.option__item_selected : ''}`}
+//                   onClick={() => setSelectedView('gallery')}
+//                 >
+//                   Галерея
+//                 </div>
+//                 <div
+//                   className={`${styles.option__item} ${selectedView === '3d' ? styles.option__item_selected : ''}`}
+//                   onClick={() => setSelectedView('3d')}
+//                 >
+//                   Смотреть в 3D
+//                 </div>
+//               </div>
+
+//               {/* Кружочки выбора */}
+//               {selectedView === 'gallery' && (
+//                 <div className={styles.info__items}>
+//                   {galleryImages.map((_, i) => (
+//                     <div
+//                       key={i}
+//                       className={`${styles.info__item} ${i === currentImageIndex ? styles.info__item_selected : ''}`}
+//                       onClick={() => handleSelectImage(i)}
+//                     />
+//                   ))}
+//                 </div>
+//               )}
+//               {selectedView === '3d' && (
+//                 <div className={styles.info__items}>
+//                   <div className={`${styles.info__item} ${styles.info__item_selected} ${styles.info__item_width}`} />
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+
+//           {/* Стрелка вправо */}
+//           {selectedView === 'gallery' && (
+//             <Arrow direction="right" onClick={handleNextImage} />
+//           )}
+//         </div>
+//       </div>
+
+//       {/* Блок цены и конфига */}
+//       <div className={styles.welcome__price}>
+//         <div className={`_container ${styles.configure}`}>
+//           <div className={styles.configure__inner}>
+//             <div className={styles.configure__material}>
+//               {colorsAndMaterials.map((item, idx) => (
+//                 <ColorConfigure
+//                   key={idx}
+//                   configureTitle={item.configureTitle}
+//                   colorData={item.colorData}
+//                   onColorSelect={(color: string) => setSelectedColor(color)}
+//                 />
+//               ))}
+//               <ButtonPrimary onClick={null}>Конфигуратор</ButtonPrimary>
+//             </div>
+
+//             <div className={`${styles.configure__price} ${styles.price}`}>
+//               <div className={styles.price__item}>
+//                 <div className={styles.configure__title}>Цена</div>
+//                 <div className={styles.price__text}>
+//                   {mainVariant.price} {mainVariant.currency}
+//                 </div>
+//                 <BonusValue bonusVal={'12 000'} />
+//               </div>
+//               <div className={`${styles.configure__btn} ${styles.btn}`}>
+//                 <ButtonOrange children="Купить" onClick={null} type="button" />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
+
+
+
 "use client"
 import React, { useState } from 'react';
 import styles from "./Welcome.module.css";
@@ -633,7 +843,21 @@ export default function Welcome({ product }: WelcomeProps) {
                 <BonusValue bonusVal={'12 000'} />
               </div>
               <div className={`${styles.configure__btn} ${styles.btn}`}>
-                <ButtonOrange children="Купить" onClick={null} type="button" />
+                <ButtonOrange
+                  onClick={() => {
+                    const storedData = localStorage.getItem("cartData");
+                    const parsedData = storedData ? JSON.parse(storedData) : { count: 0 };
+                    const newCount = parsedData.count + 1;
+                    localStorage.setItem("cartData", JSON.stringify({
+                      count: newCount,
+                      timestamp: new Date().getTime(),
+                    }));
+                    window.dispatchEvent(new Event('storage'));
+                  }}
+                  type="button"
+                  >         
+                  Купить
+                </ButtonOrange>
               </div>
             </div>
           </div>

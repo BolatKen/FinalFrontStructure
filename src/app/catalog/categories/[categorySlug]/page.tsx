241d40c76dd5287da65d292ebea3f9f6e1af7303
Bot_Kvv <bkenenbaev30@gmail.com>
2025-05-22 14:03:11 +0500

    exclude :.next/types

--- src/app/catalog/categories/[categorySlug]/page.tsx
+++ src/app/catalog/categories/[categorySlug]/page.tsx
@@ -1,38 +1,43 @@
-import { applyFilters, getCategoryFilters, getCategoryListingBySlug } from '@/services/category.service';
+import { applyFilters, getCategoryFilters, getCategoryListingBySlug } from '@/services/category.service'
 import CategoryPageClient from './CategoryPageClient';
 import parseFiltersFromUrl from '@/utils/parseFiltersFromUrl';
 
-// Типы параметров страницы (в Next 13 App Router)
-interface PageProps {
-  params: { categorySlug: string };
-  searchParams?: { page?: string; filtered?: string };
-}
 
-export default async function CategoryPage({ params, searchParams }: PageProps) {
-  const { categorySlug } = params;
-  const { filtered, page } = searchParams || {};
+type PageProps = {
+  params: {
+    categorySlug: string;
+  };
+  searchParams: {
+    page?: string;
+    filtered?: string;
+  };
+};
+
+export default async function CategoryPage({ params, searchParams }: PageProps
+  ) {
+  const { categorySlug } = await params;
+  const searchParamsResolved = await searchParams;
+  const { filtered, page } = searchParamsResolved;
   const currentPage = parseInt(page || '1', 10);
 
   const categoryData = await getCategoryListingBySlug(categorySlug, currentPage);
   if (!categoryData) {
     return <div>Категория не найдена</div>;
   }
-
   const categoryFilters = await getCategoryFilters();
+
   const parsedFilters = parseFiltersFromUrl(filtered);
 
   const products = Object.keys(parsedFilters).length
     ? await applyFilters(parsedFilters, categorySlug, currentPage)
     : categoryData.products;
 
-  return (
-    <CategoryPageClient
-      category={categoryData}
-      products={products || []}
-      currentPage={currentPage}
-      categoryFilters={categoryFilters}
-      categorySlug={categoryData.slug}
-      initialFilters={parsedFilters}
-    />
-  );
+
+  return <CategoryPageClient
+    category={categoryData}
+    products={products || []}
+    currentPage={currentPage}
+    categoryFilters={categoryFilters}
+    categorySlug={categoryData.slug}
+    initialFilters={parsedFilters} />;
 }

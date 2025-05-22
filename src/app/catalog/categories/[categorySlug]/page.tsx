4dc052e3b7030b7eee06c1cd276d183e323c5e4d
Bot_Kvv <bkenenbaev30@gmail.com>
2025-05-22 13:20:46 +0500

    problems

--- src/app/catalog/categories/[categorySlug]/page.tsx
+++ src/app/catalog/categories/[categorySlug]/page.tsx
@@ -1,57 +1,16 @@
-// import { applyFilters, getCategoryFilters, getCategoryListingBySlug } from '@/services/category.service';
-// import CategoryPageClient from './CategoryPageClient';
-// import parseFiltersFromUrl from '@/utils/parseFiltersFromUrl';
-
-// interface CategoryPageProps {
-//   params: { categorySlug: string };
-//   searchParams: { page?: string; filtered?: string };
-// }
-
-// export default async function CategoryPage({
-//   params,
-//   searchParams,
-// }: CategoryPageProps) {
-//   const { categorySlug } = params;
-//   const { filtered, page } = searchParams;
-//   const currentPage = parseInt(page || '1', 10);
-
-//   const categoryData = await getCategoryListingBySlug(categorySlug, currentPage);
-//   if (!categoryData) {
-//     return <div>Категория не найдена</div>;
-//   }
-
-//   const categoryFilters = await getCategoryFilters();
-//   const parsedFilters = parseFiltersFromUrl(filtered);
-
-//   const products = Object.keys(parsedFilters).length
-//     ? await applyFilters(parsedFilters, categorySlug, currentPage)
-//     : categoryData.products;
-
-//   return (
-//     <CategoryPageClient
-//       category={categoryData}
-//       products={products || []}
-//       currentPage={currentPage}
-//       categoryFilters={categoryFilters}
-//       categorySlug={categoryData.slug}
-//       initialFilters={parsedFilters}
-//     />
-//   );
-// }
-
 import { applyFilters, getCategoryFilters, getCategoryListingBySlug } from '@/services/category.service';
 import CategoryPageClient from './CategoryPageClient';
 import parseFiltersFromUrl from '@/utils/parseFiltersFromUrl';
 
-export default async function CategoryPage({
-  params,
-  searchParams = {},
-}: {
+// Типы параметров страницы (в Next 13 App Router)
+interface PageProps {
   params: { categorySlug: string };
   searchParams?: { page?: string; filtered?: string };
-}) {
+}
+
+export default async function CategoryPage({ params, searchParams }: PageProps) {
   const { categorySlug } = params;
-  const { filtered, page } = searchParams;
+  const { filtered, page } = searchParams || {};
   const currentPage = parseInt(page || '1', 10);
 
   const categoryData = await getCategoryListingBySlug(categorySlug, currentPage);
@@ -77,4 +36,3 @@ export default async function CategoryPage({
     />
   );
 }
-

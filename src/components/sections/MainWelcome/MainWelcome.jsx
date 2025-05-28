9350d3e4d5cf2191d83f39a48f4ecc8da1191707
yanekissayabully <aripov.cr7@gmail.com>
2025-05-28 16:57:14 +0500

    fixmainwelcome

--- src/components/sections/MainWelcome/MainWelcome.jsx
+++ src/components/sections/MainWelcome/MainWelcome.jsx
@@ -120,7 +120,7 @@ const handleNext = () => {
         onTouchMove={onTouchMove}
         onTouchEnd={onTouchEnd}
       >
-        <div className={styles.desktop}>
+        {/* <div className={styles.desktop}>
           <div className={[styles.welcome__img, "_img"].join(" ")}>
             <div key={currentProduct?.image} className={styles.imageWrapper}>
               {currentProduct?.image && (
@@ -128,9 +128,9 @@ const handleNext = () => {
               )}
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={styles.mobile}>
+        {/* <div className={styles.mobile}>
           <div className={styles.slider}>
             <div
               className={styles.sliderInner}
@@ -148,15 +148,34 @@ const handleNext = () => {
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
         {/* <div className={[styles.welcome__img, '_img'].join(' ')}>
                     {currentProduct?.image && (
                         <img src={currentProduct.image} alt="Фото" />
                     )}
                 </div> */}
         <Header isBlur={false} />
-        <div className={styles.welcome__inner}>
-          <div className={[styles.welcome__description, styles.desc].join(" ")}>
+<div className={styles.welcome__inner}>
+<div className={styles.welcome__img}>
+  <div className={styles.imageWrapper}>
+    {currentProduct?.image && (
+      <Image
+        src={currentProduct.image}
+        alt="Фото"
+        layout="responsive"
+        width={100}
+        height={100}
+        objectFit="contain"
+        priority
+      />
+    )}
+  </div>
+</div>
+
+
+
+  <div className={[styles.welcome__description, styles.desc].join(" ")}>
+
             <div
               className={["_container-bigger", styles.desc__inner].join(" ")}
             >

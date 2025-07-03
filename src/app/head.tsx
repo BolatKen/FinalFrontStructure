a8e3cfe4b4bd60d4a96b3bb77ea591dc0b2538a5
Bot_Kvv <bkenenbaev30@gmail.com>
2025-07-03 08:25:50 +0500

    still fixes, not favicon not showing

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+// filepath: src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Leka Beauty</title>
+      <meta name="description" content="Сайт по обстановке барбершопов." />
+      <link rel="icon" href="/favicon.svg" />
+    </>
+  );
+}
